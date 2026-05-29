6dadaff0b1fe25f7505457aa3f4d95cebbbeb957
WuiiiGithub <142606875+WuiiiGithub@users.noreply.github.com>
2026-05-29 16:34:22 +0530

    Added basic details and defs

--- dseshpy/__init__.py
+++ dseshpy/__init__.py
@@ -8,3 +8,25 @@ Package under development. ^.^
 ---
 Thank you
 """
+
+class SessionManager:
+    """
+    A discord study session needs to be managed properly. 
+    Thus requiring a need of session manager.
+    Individual sessions would be just like random in memory otherwise.
+
+    Sessions can be managed at multiple levels.
+    - Global Level
+    - Guild Level
+
+    Despite of what level, 
+    Session Managers have certain properties and functionalities.
+
+    Properties like:
+    ---
+    - session_count
+    (lets just keep this for now)
+
+    Functionalities like:
+    - sync() : To sync to database
+    """
---
+++ dseshpy/session.py
@@ -0,0 +1,27 @@
+"""
+session
+===
+Contains session details 
+"""
+
+class Session:
+    """
+    A session refers to a study session in discord vc
+    It is uniquely known with its 
+    It has various properties and functionalities
+
+    Properties:
+    ---
+    - owner_id
+    - guild_id
+    - channel_id
+    - members_count
+    - member_limit
+    - vc_level
+    - vc_xp
+    - rent_type
+    - rent_amount
+    - isCamSession
+    - isScreenShareSession
+    """
+    pass
