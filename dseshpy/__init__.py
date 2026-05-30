1254c995c043f68e05e1417c7d27f01f86efbcc3
WuiiiGithub <142606875+WuiiiGithub@users.noreply.github.com>
2026-05-30 18:44:34 +0530

    Include session information init

--- dseshpy/__init__.py
+++ dseshpy/__init__.py
@@ -8,6 +8,13 @@ Package under development. ^.^
 ---
 Thank you
 """
+from pymongo.collection import Collection as _Collection
+collections = {}
+
+def initialize(
+    session_collection: _Collection
+):
+    collections['session'] = session_collection
 
 class SessionManager:
     """
--- dseshpy/session.py
+++ dseshpy/session.py
@@ -24,4 +24,31 @@ class Session:
     - isCamSession
     - isScreenShareSession
     """
-    pass
+    def __init__(
+        self,
+        owner_id: str,
+        guild_id: str, 
+        channel_id: str,
+        members_count: dict,
+        members_limit: int,
+        vc_level: int,
+        vc_xp: int,
+        rent_type: str,
+        rent_amount: int,
+        isCamSession: bool,
+        isScreenShareSession: bool
+    ):
+        self.owner_id = owner_id
+        self.guild_id = guild_id
+        self.channel_id = channel_id
+        self.members_count = members_count
+        self.members_limit = members_limit
+        self.vc_level = vc_level
+        self.vc_xp = vc_xp
+        self.rent_type = rent_type
+        self.rent_amount = rent_amount
+        self.isCamSession = isCamSession
+        self.isScreenShareSession = isScreenShareSession
+
+    def dropperRoutine(self):
+        pass
